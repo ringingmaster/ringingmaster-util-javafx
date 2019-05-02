e8dbeb686b22a7d0376b8b9d8ad0db4ef08b8bf7
Steve Lake <stevelake@Steve-Lake-MacBook-Pro.local>
2019-05-02 09:13:49 +0100

    More work on the properties window.

--- src/main/java/org/ringingmaster/util/javafx/propertyeditor/IntegerPropertyValue.java
+++ src/main/java/org/ringingmaster/util/javafx/propertyeditor/IntegerPropertyValue.java
@@ -3,12 +3,9 @@ package org.ringingmaster.util.javafx.propertyeditor;
 import com.google.common.base.Objects;
 import com.google.common.base.Strings;
 import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
@@ -29,7 +26,7 @@ public class IntegerPropertyValue extends SkeletalPropertyValue implements Prope
     private CallbackStyle callbackStyle = CallbackStyle.EVERY_KEYSTROKE;
     private Integer previousNotifiedValue = null;
     private Integer currentValue = null;
-    private ChangeListener<? super Number> listener;
+    private ChangeListener<? super Integer> listener;
 
     public IntegerPropertyValue(String propertyName) {
         super(propertyName);
@@ -43,40 +40,34 @@ public class IntegerPropertyValue extends SkeletalPropertyValue implements Prope
         //TODO textField.setPromptText("TEST");
 
         // Prevent anything other than integer numbers
-        textField.setOnKeyTyped(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent event) {
-                if ((event.getCharacter() != null) &&
-                        (event.getCharacter().length() > 0) &&
-                        (!Character.isDigit(event.getCharacter().charAt(0)))) {
-                    event.consume();
-                }
+        textField.setOnKeyTyped(event -> {
+            if ((event.getCharacter() != null) &&
+                    (event.getCharacter().length() > 0) &&
+                    (!Character.isDigit(event.getCharacter().charAt(0)))) {
+                event.consume();
             }
         });
 
         // update psudo value when valid
-        textField.textProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue<? extends String> observableValue, String oldValue, String newValue) {
-                try {
-                    if (Strings.isNullOrEmpty(newValue)) {
-                        currentValue = null;
-                    } else {
-                        currentValue = Integer.parseInt(newValue);
-                    }
-                } catch (NumberFormatException e) {
-                    // Do nothing
+        textField.textProperty().addListener((observableValue, oldValue, newValue) -> {
+            try {
+                if (Strings.isNullOrEmpty(newValue)) {
+                    currentValue = null;
+                } else {
+                    currentValue = Integer.parseInt(newValue);
                 }
-                if (callbackStyle == CallbackStyle.EVERY_KEYSTROKE) {
-                    notifyListener();
-                }
+            } catch (NumberFormatException e) {
+                // Do nothing
+            }
+            if (callbackStyle == CallbackStyle.EVERY_KEYSTROKE) {
+                notifyListener();
             }
         });
 
         return textField;
     }
 
-    public void setListener(ChangeListener<? super Number> listener, CallbackStyle callbackStyle) {
+    public void setListener(ChangeListener<? super Integer> listener, CallbackStyle callbackStyle) {
 
         this.callbackStyle = callbackStyle;
         this.listener = listener;
@@ -116,10 +107,9 @@ public class IntegerPropertyValue extends SkeletalPropertyValue implements Prope
                 textField.setText(convertedValue);
             }
         }
-        // Need to do this, otherwise this change never gets notified as no keyboard action takes place.
-        if (callbackStyle == CallbackStyle.WHEN_FINISHED) {
-            notifyListener();
-        }
+        // It would be perverse to notify something the caller has set.
+        // This ensures that the the next change is properly notified
+        previousNotifiedValue = value;
     }
 
     private void notifyListener() {
